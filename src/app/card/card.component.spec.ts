4e50c4c41cade3e410179c2cc2c31c2017a56f9a
abhinavpraneeth <sabhinavpraneeth@gmail.com>
2021-10-07 07:11:04 +0530

    improved styling and testing the card component

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -23,4 +23,38 @@ describe('CardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have card element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.card')).toBeTruthy();
+  });
+
+  it('should have header element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.header')).toBeTruthy();
+  });
+  it('should have image thumbnail element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.thumbnail')).toBeTruthy();
+  });
+
+  it('should have title element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.title')).toBeTruthy();
+  });
+
+  it('should have height element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.height')).toBeTruthy();
+  });
+
+  it('should have weight element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.weight')).toBeTruthy();
+  });
+  it('should have abilities element', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.abilities-title')).toBeTruthy();
+    expect(compiled.querySelector('.abilities')).toBeTruthy();
+  });
 });
